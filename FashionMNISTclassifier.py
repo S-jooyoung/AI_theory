b58e66fdeb62e569ec9c808e412274c751d93793
S-jooyoung <jooyoung.dev@gmail.com>
2021-12-06 20:00:00 +0000

    Feat:FashionMNIST분류 데이터 불러오기 및 확인

--- FashionMNISTclassifier.py
+++ FashionMNISTclassifier.py
@@ -6,3 +6,16 @@ import matplotlib.pyplot as plt
 #데이터 불러오기
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data() 
+
+#데이터 이미지 확인
+class_names = ["T-shirt/top" , "Trouser", "Pullover", "Dress", "Coat",
+               "Sandal" , "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+plt.figure(figsize=(10,5))
+
+for c in range(5):
+    plt.subplot(1,5,c+1)
+    plt.imshow(X_train_full[c], cmap ="gray")
+    plt.title(class_names[y_train_full[c]])
+    plt.axis("off")
+plt.show()
